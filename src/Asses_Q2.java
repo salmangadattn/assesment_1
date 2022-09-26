4c6d66b285f3ac12332a0cb38bee107d68751e9e
salmangadattn <salman.gada@tothenew.com>
2022-09-26 21:56:30 +0530

    modified comments

--- src/Asses_Q2.java
+++ src/Asses_Q2.java
@@ -3,20 +3,19 @@ import java.util.Scanner;
 
 public class Asses_Q2 {
 
-    /*Hi my name is salm
+    /*
     Q2) Write a program to read user input until user writes XDONE
     and then show the entered text by the user on commandline
     */
     public static void main(String[] args) {
 
 
-
         Scanner sc = new Scanner(System.in);
 
         ArrayList<String> lines = new ArrayList<>();
 
-        while(sc.hasNextLine()){
-            String line =  sc.nextLine();
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
 
             if (line.equals("XDONE")) {
                 break;
@@ -24,7 +23,7 @@ public class Asses_Q2 {
             lines.add(line);
         }
 
-        for(String line:lines){
+        for (String line : lines) {
             System.out.println(line);
         }
 
